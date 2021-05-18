62a6b92d787ea59e2ced119ac6485c1776ec9ed3
Belal Quamar <belalquamar@gmail.com>
2021-05-19 01:17:05 +0530

    Update index.php

--- css/index.php
+++ css/index.php
@@ -1,5 +1,5 @@
 
 <?php
 $link = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=".$_GET["d"]."&date=".date('d-m-Y');
-$comm = "curl --request GET '".$link."' --header 'Accept-Language: hi_IN' --header 'Accept: application/json' --header 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Saf$
+$comm = "curl --request GET '".$link."' --header 'Accept-Language: hi_IN' --header 'Accept: application/json' --header 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'";
 echo shell_exec($comm);
